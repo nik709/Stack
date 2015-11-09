a711ff9eca05981eda4dfaad7a7c60f17c21ed15
nik709 <Nikitagrechukhin@gmail.com>
2015-11-09 21:34:58 +0300

    Add Parser

---
+++ Tparser.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "TStack.h"
+
+using namespace std;
+
+#define MaxLen 201
+
+class Tparser
+{
+private:
+	char inf[MaxLen];
+	char post[MaxLen];
+	Tstack <double> st_d;
+	Tstack <double> st_c;
+public:
+};
